329d81476afc20feef5361d411874485e9a6aa4e
lawrence-254 <lodhiambo26@gmail.com>
2024-07-29 19:46:07 +0300

    formSetup

--- myApp/auth.py
+++ myApp/auth.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    username=request.form.get("inputUserName3")
+    email=request.form.get("inputEmail3")
+    password=request.form.get("inputPassword3")
+    confirm_password=request.form.get("inputConfirmPassword3")
+    print(username, email, password, confirm_password)
     return render_template("register.html", title="REGISTER")
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    email=request.form.get("inputEmail3")
+    password=request.form.get("inputPassword3")
+    print(email, password)
     return render_template("login.html", title="LOGIN")
 
 @auth.route("/logout")
